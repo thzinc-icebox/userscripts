c0d40395acfa6fe99f95d507ca1205f1260d0923
Daniel James <daniel@thzinc.com>
2015-02-23 10:43:53 -0800

    Added Bitbucket user script for using the ignore white space option

---
+++ Bitbucket/Ignore_white_space_option.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name        Ignore whitespace option on Bitbucket
+// @namespace   com.thzinc.bitbucket
+// @include     https://bitbucket.org/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$(function () {
+  $('.pullrequest-list .title a')
+   .each(function () {
+     var self = $(this);
+     var sensitive = self.attr('href');
+     var insensitive = sensitive + '?w';
+     self.data('sensitive', sensitive);
+     self.data('insensitive', insensitive);
+     self.attr('href', self.data(localStorage['ignore-whitespace'] ? 'insensitive' : 'sensitive'));
+   });
+  
+  var checkbox = $('<input type="checkbox" id="ignore-whitespace"/>')
+   .prop('checked', !!localStorage['ignore-whitespace'])
+   .on('change', function () {
+     if ($(this).prop('checked'))
+      localStorage['ignore-whitespace'] = true;
+     else
+       delete localStorage['ignore-whitespace'];
+     
+     $('.pullrequest-list .title a')
+      .each(function () {
+        var self = $(this);
+        self.attr('href', self.data(localStorage['ignore-whitespace'] ? 'insensitive' : 'sensitive'));
+      })
+   });
+  
+  var label = $('<label for="ignore-whitespace"/>')
+   .append(checkbox)
+   .append("Ignore White Space");
+  
+  var li = $('<li/>').append(label);
+  $('.filter-container .filter-status').append(li);
+});
+
+$(function () {
+  var isWhiteSpaceIgnored = false;
+  var search = location.search
+   .split(/[\?\&]+/g)
+   .filter(function (e) {
+     var x = /^w=/.test(e);
+     isWhiteSpaceIgnored = isWhiteSpaceIgnored || x;
+     return !x;
+   })
+   .join('&');
+  var baseUrl = location.pathname + '?' + search;
+  var button = $('<a class="aui-button">')
+   .attr('href', isWhiteSpaceIgnored ? baseUrl : baseUrl + 'w=')
+   .text(isWhiteSpaceIgnored ? 'View White Space Differences' : 'Ignore White Space');
+
+  $('#pullrequest-actions')
+   .append($('<div class="aui-buttons">').append(button));
+});
---
+++ Bitbucket/README.md
@@ -0,0 +1,9 @@
+Bitbucket User Scripts
+======================
+
+Scripts for using [Bitbucket](https://bitbucket.org).
+
+Ignore White Space Option
+-------------------------
+
+Bitbucket has an option in beta to ignore white space on a PR. This is super useful. [`Ignore_white_space_option.js`](Ignore_white_space_option.js)
